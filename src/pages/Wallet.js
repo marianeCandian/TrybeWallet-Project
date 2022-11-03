c07dad823ae60ea057bd8b7529eaadcb6b7559d5
Mariane Candian <mariane.candian@gmail.com>
2022-11-03 17:47:16 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
 import { getRequest } from '../redux/actions';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   componentDidMount() {
@@ -16,6 +17,7 @@ class Wallet extends React.Component {
       <>
         <Header />
         <WalletForm />
+        <Table />
       </>
     );
   }
